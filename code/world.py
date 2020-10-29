9bf78a45c0a4aee21db8f358f3c13ad6001b9be3
jianbaiye <gusye666@gmail.com>
2020-10-29 17:04:41 +0800

    fix dataset' name

--- code/world.py
+++ code/world.py
@@ -28,7 +28,7 @@ if not os.path.exists(FILE_PATH):
 
 
 config = {}
-all_dataset = ['lastfm', 'gowalla', 'yelp2018', 'amazon']
+all_dataset = ['lastfm', 'gowalla', 'yelp2018', 'amazon-book']
 all_models  = ['mf', 'lgn']
 # config['batch_size'] = 4096
 config['bpr_batch_size'] = args.bpr_batch
