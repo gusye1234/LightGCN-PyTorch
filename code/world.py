4ec54d9fd35a93878922a64efb91552d274f3379
Hozayfa El Rifai <p-p-h@hotmail.com>
2023-03-20 11:40:51 +0400

    returning the directory containing the script instead of CWD

--- code/world.py
+++ code/world.py
@@ -16,7 +16,7 @@ import multiprocessing
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 args = parse_args()
 
-ROOT_PATH = os.getcwd()
+ROOT_PATH = os.path.dirname(os.path.dirname(__file__))
 CODE_PATH = join(ROOT_PATH, 'code')
 DATA_PATH = join(ROOT_PATH, 'data')
 BOARD_PATH = join(CODE_PATH, 'runs')
